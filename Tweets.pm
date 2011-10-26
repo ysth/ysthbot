e44c7bdbd4d1de071289f70f1af8aa5621809c5b
Ivan Heffner <iheffner@gmail.com>
2011-10-25 17:00:54 -0700

    initial commit

---
+++ Tweets.pm
@@ -0,0 +1,60 @@
+package Bot::BasicBot::Pluggable::Module::Tweets;
+
+use strict;
+use Bot::BasicBot::Pluggable::Module;
+use base qw(Bot::BasicBot::Pluggable::Module);
+
+use LWP::Simple ();
+use JSON::XS ();
+
+my $api_url = 'http://api.twitter.com/1/statuses/show/%d.json';
+my $response = '@%s: %s';
+sub said {
+    my ($self, $mess, $pri) = @_;
+
+    my $body = $mess->{body};
+    my $who  = $mess->{who};
+
+    warn "$who said $body with priority $pri\n";
+    return unless ($pri == 2);
+    return unless my($user,$tid) = ($body =~ m{https?://twitter.com/\#!/([^/]+)/status/(\d+)});
+
+    my $url = sprintf($api_url, $tid);
+    warn "get $url";
+    my $tweet_json = LWP::Simple::get( $url );
+    warn $tweet_json;
+
+    my $tweet = JSON::XS->decode( $tweet_json );
+
+    return sprintf( $response, $tweet->{user}{screen_name}, $tweet->{text} );
+}
+
+sub help {
+    return 'You had me at "twitter.com"';
+}
+
+1;
+
+__END__
+
+=head1 NAME
+
+Bot::BasicBot::Pluggable::Module::Tweets - parses links to twitter status updates, returns the text of the update
+
+=head1 IRC USAGE
+
+    http://twitter.com/#!/$user/status/$id
+
+=head1 AUTHOR
+
+Ivan Heffner <iheffner+tweets@gmail.com>
+
+=head1 COPYRIGHT
+
+Copyright 2011, Ivan Heffner
+
+Distributed under the same terms as Perl itself.
+
+=head1 SEE ALSO
+
+=cut
